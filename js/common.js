963553ebd96b0dd3c2c3bdfd161c22489ff75dd4
ksyPublic <rlatpdud0309@gmail.com>
2021-11-29 02:48:40 +0900

    메인 제작중

--- js/common.js
+++ js/common.js
@@ -1,17 +1,28 @@
 //ES5 ~ ES6
 (function (window) {
     const body = document.body;
+    const nav = document.querySelector(".nav");
+    const navList = nav.querySelectorAll("li");
 
     //default ui Interective
     const ui = {
         web: function () {
             console.log("WEB", body);
         },
+
+        animation: function () {
+            if (nav) {
+                [].forEach.call(navList, function (item, index) {
+                    item.classList.add("on");
+                });
+            }
+        },
     };
 
     //init
     function init() {
         ui.web();
+        ui.animation();
     }
 
     //document load
