b3feeb26b3c8f857ac1b50e65958ed3b2539c702
ksyPublic <rlatpdud0309@gmail.com>
2022-01-02 02:57:36 +0900

    폴리필 추가

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
 //ES5 ~ ES6
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+        var el = this;
+
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
+
 (function (window) {
     //즉시 실행함수
     const nav = document.querySelector(".nav");
